91878da771700e58e312a18045243094be00ad6c
Chrisotpher Luey <cluey21@csus.org>
2018-10-20 18:27:11 -0700

    10/20/18 6:27

--- MyBot.java
+++ MyBot.java
@@ -1,43 +1,45 @@
-// This Java API uses camelCase instead of the snake_case as documented in the API docs.
-//     Otherwise the names of methods are consistent.
-
 import hlt.*;
-import neuralnet.*;
+import NeuralNet.Matrix;
+import NeuralNet.NeuralNet;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MyBot {
+    //static NeuralNet brain;
     public static void main(final String[] args) {
+        //brain = new NeuralNet(24, 18, 4);
         final long rngSeed;
         if (args.length > 1) {
             rngSeed = Integer.parseInt(args[1]);
         } else {
             rngSeed = System.nanoTime();
         }
-        final Random rng = new Random(rngSeed);//Show me the neuralnet file plz
+
+        final Random rng = new Random(rngSeed);
 
         Game game = new Game();
         // At this point "game" variable is populated with initial map data.
         // This is a good place to do computationally expensive start-up pre-processing.
         // As soon as you call "ready" function below, the 2 second per turn timer will start.
         game.ready("BaconD");
-
         Log.log("Successfully created bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
 
-        for (;;) {
+        while(true) {
             game.updateFrame();
             final Player me = game.me;
             final GameMap gameMap = game.gameMap;
-//
+
             final ArrayList<Command> commandQueue = new ArrayList<>();
 
             for (final Ship ship : me.ships.values()) {
                 if (gameMap.at(ship).halite < Constants.MAX_HALITE / 10 || ship.isFull()) {
                     final Direction randomDirection = Direction.ALL_CARDINALS.get(rng.nextInt(4));
                     commandQueue.add(ship.move(randomDirection));
+                    Log.log(String.valueOf(randomDirection));
                 } else {
                     commandQueue.add(ship.stayStill());
+                    Log.log("Ship stayed still");
                 }
             }
 
--- run_game.sh
+++ run_game.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
-
 javac MyBot.java
+javac NeuralNet/Matrix.java
+javac NeuralNet/NeuralNet.java
 ./halite --replay-directory replays/ -vvv --width 32 --height 32 "java MyBot" "java MyBot"
