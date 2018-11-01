4cdc2932f5ed5c61d6c5eb28d42ef0b21d4b3913
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2018-11-01 14:20:13 -0700

    idk what I was doing but I need to commit so whatevs

--- MyBot.java
+++ MyBot.java
@@ -6,8 +6,9 @@ import java.util.Random;
 
 public class MyBot {
     public static void main(String[] args) {
-        Random rng = getRNG(args);
+        Random rng = new Random();
         Config config = new Config(/*16, */randomInt(rng, 75, 125), randomInt(rng, 100, 250), randomInt(rng, 300, 750), randomInt(rng, 50, 500));
+        rng = getRNG(args);
         Game game = new Game();
         Plantain bot = new Plantain(rng, game, config);
         bot.initialize();
@@ -29,6 +30,7 @@ public class MyBot {
         } else {
             rngSeed = System.nanoTime();
         }
+        rngSeed = 1;
         Log.log("Random Seed: " + rngSeed);
         return new Random(rngSeed);
     }
