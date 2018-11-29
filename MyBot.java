7698ca422c452bbb568d9d8d8ac186bf2594866d
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2018-11-29 15:18:35 -0800

    Revert "Revert "Merge pull request #2 from MajikalExplosions/BaconD""
    This reverts commit cefc0a9d08fb24e0d549b6be294edcecd6566882.

--- MyBot.java
+++ MyBot.java
@@ -1,7 +1,5 @@
-// This Java API uses camelCase instead of the snake_case as documented in the API docs.
-//     Otherwise the names of methods are consistent.
-
 import hlt.*;
+import NeuralNet.*;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,17 +12,14 @@ public class MyBot {
         } else {
             rngSeed = System.nanoTime();
         }
+
         final Random rng = new Random(rngSeed);
 
         Game game = new Game();
-        // At this point "game" variable is populated with initial map data.
-        // This is a good place to do computationally expensive start-up pre-processing.
-        // As soon as you call "ready" function below, the 2 second per turn timer will start.
-        game.ready("MyJavaBot");
-
+        game.ready("BaconD");
         Log.log("Successfully created bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
 
-        for (;;) {
+        while(true) {
             game.updateFrame();
             final Player me = game.me;
             final GameMap gameMap = game.gameMap;
